472bc36e71db4f1957f34a52396e14f6254f3873
RageBrain <Ljashenykviktor@gmail.com>
2017-06-08 20:43:26 +0300

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   mount ActionCable.server => '/cable'
-  resources :posts, only: [:index]
+  resources :posts
   resources :chat_rooms, only: [:new, :create, :show, :index]
   get :send_emotions, to: 'chat_rooms#send_emotions'
 end
